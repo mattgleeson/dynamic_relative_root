84354e021359a955f46e17038525b7b551e8e59d
Matt Gleeson <matt@gleeson.org>
2010-09-10 09:19:54 -0700

    add current_root

--- lib/dynamic_relative_root.rb
+++ lib/dynamic_relative_root.rb
@@ -3,6 +3,8 @@ class DynamicRelativeRoot
   cattr_accessor :matcher
   @@current = nil
   cattr_accessor :current
+  @@current_root = nil
+  cattr_accessor :current_root
   attr_accessor :root
 
   def initialize(request_path)
@@ -17,6 +19,16 @@ class DynamicRelativeRoot
       @request_path.sub(@root, '')
     else
       @request_path
+    end
+  end
+
+  def current_root
+    if @@current_root
+      @@current_root
+    elsif @@current
+      @@current.root
+    else
+      ''
     end
   end
 end
@@ -39,8 +51,8 @@ end
 class ActionController::Routing::RouteSet
   def generate_with_dynamic_relative_root(*args)
     path = generate_without_dynamic_relative_root(*args)
-    if path && path.is_a?(String) && DynamicRelativeRoot.current
-      path = DynamicRelativeRoot.current.root + path
+    if path && path.is_a?(String) && DynamicRelativeRoot.current_root
+      path = DynamicRelativeRoot.current_root + path
     end
     path
   end
